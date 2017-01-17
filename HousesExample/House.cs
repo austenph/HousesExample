e82d4a103d1212a5cf534f1dbcf555e9565f3d0a
Austen Perelman-Hall <austenph@gmail.com>
2017-01-17 13:54:51 -0500

    Added four private fields to class House

---
+++ HousesExample/House.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousesExample
+{
+    class House //class declaration and name, default access level is public
+    {
+        //these are all fields below:
+        private string color;
+        private int rooms;
+        private int size;
+        private int doors;
+    }
+}
